649d63b83e15107e02fff72aef2d06e30df0aaa5
Yousef Mardrosyan <yousefm2315@gmail.com>
2023-05-29 19:14:55 -0400

    move to netbeans

--- csi2999/src/main/java/Main/Csi2999.java
+++ csi2999/src/main/java/Main/Csi2999.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package Default_Packages;
+package Main;
 
-import Guis.*;
+import Guis.Login;
 
 /**
  *
