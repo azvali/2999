a3c9688819d1dbce4ec4e4bbfd4b4857c9c4da5d
Yousef Mardrosyan <yousefm2315@gmail.com>
2023-05-29 17:54:50 -0400

    move to netbeans

---
+++ csi2999/src/main/java/com/mycompany/csi2999/Csi2999.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.csi2999;
+
+/**
+ *
+ * @author azval
+ */
+public class Csi2999 {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
