0e34bc0272690853aa6b60cbf659884eec191cf8
Yousef Mardrosyan <yousefm2315@gmail.com>
2023-05-29 19:09:01 -0400

    move to netbeans

--- csi2999/src/main/java/Default_Packages/Csi2999.java
+++ csi2999/src/main/java/Default_Packages/Csi2999.java
@@ -2,7 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.csi2999;
+package Default_Packages;
+
+import Guis.*;
 
 /**
  *
@@ -11,6 +13,7 @@ package com.mycompany.csi2999;
 public class Csi2999 {
 
     public static void main(String[] args) {
-        System.out.println("macbook commit");
+        Login login = new Login(); 
+        login.setVisible(true);
     }
 }
